9ebee41424f5689de9c31e2871792d4a819e810e
Gabriel Chagas <gabrielserq@outlook.com>
2019-09-20 15:36:18 -0300

    testando user

--- 0001-helloWorld/Program.cs
+++ 0001-helloWorld/Program.cs
@@ -6,14 +6,10 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
-            
             var dia = new Dia();
-            
             int hora = DateTime.Now.Hour;//recuperando a hora atual
-
             System.Console.Write("Digite seu nome: ");
             string nome = Console.ReadLine();//lendo o nome dá pessoa
-
             if(hora < 12){
                 Console.WriteLine($"Bom dia {nome}, no momento são {dia.retornaDia().ToShortTimeString()} do dia {dia.retornaDia().ToShortDateString()}");//se o dia for menor que 12, imprime bom dia 
             }else if (hora < 18){
