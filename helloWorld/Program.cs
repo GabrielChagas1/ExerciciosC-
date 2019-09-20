c63a25f57b9a3b08c4a044ba2f2d82585bbbfc6a
Gabriel Serqueira Chagas <gabriel.chagas@seepix.com.br>
2019-09-20 10:09:35 -0300

    Primeiro Commit|

---
+++ helloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia = DateTime.Now.Hour;//recuperando a hora atual
+
+            System.Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();//lendo o nome dá pessoa
+
+            if(dia < 12){
+                Console.WriteLine($"Bom dia {nome}");//se o dia for menor que 12, imprime bom dia 
+            }else if (dia < 18){
+                Console.WriteLine($"Boa tarde {nome}");//se o dia for menor que 18, imprime boa tarde
+            }else{
+                System.Console.WriteLine($"Boa noite {nome}");//se o dia for maior que 18, imprime boa noite
+            }
+        }
+    }
+}
