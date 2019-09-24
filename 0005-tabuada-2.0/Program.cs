b1038fe710316a65080afcb4d082f67b059f580e
Gabriel Chagas <gabrielserq@outlook.com>
2019-09-24 16:53:01 -0300

    Segunda versão da tabuada

---
+++ 0005-tabuada-2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _0005_tabuada_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabuada = 0;
+            do
+            {
+                System.Console.Write("Tabuada: ");
+                tabuada = int.Parse(Console.ReadLine());//recebendo o número da tabuada
+                new Tabuada().MontarTabuada(tabuada);//chamando a função para montar e exibir a tabuada
+
+            } while (tabuada >=0);
+            System.Console.WriteLine("Obrigado e volte sempre!");
+        }
+    }
+}
