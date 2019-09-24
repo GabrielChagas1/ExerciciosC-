d53ddfea4a5d83bffba5183e3e4ca341704c47cb
Gabriel Chagas <gabrielserq@outlook.com>
2019-09-24 16:15:09 -0300

    Aprendendo a ler arquivos

---
+++ 0003-LendoArquivos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace _0003_LendoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa para aprender a ler arquivos
+
+            foreach (string line in File.ReadLines(@"c:\Users\gabriel\Desktop\LendoArquivo.txt"))
+            {
+                System.Console.WriteLine(line);
+            }
+        }
+    }
+}
